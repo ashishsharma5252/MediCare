1b417985d97372f7bf1b7716f291d4da01351327
ashishsharma5252 <sharmajii87080@gmail.com>
2026-04-07 15:33:08 +0530

    Mongodb uri

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://Ashish:0ikzS4Glj4jqk05D@cluster0.57gyvuc.mongodb.net/MediCare").then(()=>{
-        console.log("Connected to MongoDB");
-    })
-}
+  await mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log("Connected to MongoDB");
+  });
+};
